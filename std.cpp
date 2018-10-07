9ce99e132e0c272f98922a21b29a625ee98ec9dc
300iq <43923450+300iq@users.noreply.github.com>
2018-10-07 09:36:17 +0300

    Create std.cpp

---
+++ std.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <list>
+#include <time.h>
+#include <math.h>
+#include <random>
+#include <deque>
+#include <queue>
+#include <cassert>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+
+mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int main()
+{
+#ifdef ONPC
+    freopen("a.in", "r", stdin);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
